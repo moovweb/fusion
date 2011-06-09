fd8ffcbdae9bdd86dc7acb1d553d20d34d7344b0
Sean Jezewski <sean@moovweb.com>
2011-06-09 12:47:01 -0700

    Initial commit -- made fusion into a gem and built it

---
+++ fusion.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |s|
+  s.name = "fusion"
+  s.version = "0.0.1"
+  s.platform = Gem::Platform::RUBY
+
+  s.authors = ["Sean Jezewski"]
+  s.email = ["sean@moovweb.com"]
+
+  s.homepage = "http://github.com/moovweb/fusion"
+  s.summary = "Simple javascript bundler plugin."
+  s.description = "Fusion bundles and re-bundles your javascript in two modes - quick (dumb concatenation) and optimized (google closure compiler's SIMPLE_OPTIMIZATIONS level"
+
+  s.files = Dir['README.md','lib/*','compiler/*']
+  s.require_path = "lib"
+  
+  s.add_dependency "open4"
+end
---
+++ lib/fusion.rb
@@ -0,0 +1,108 @@
+require 'yaml'
+require 'open4'
+
+module Fusion
+
+  def configure(options)
+    @options ||= {}
+    @options.update(options)
+
+    raise Exception("Configuration error -- must specify #{:bundle_file_path} when configuring Fusion javascript bundler") if @options[:bundle_file_path].nil?
+
+    @options[:project_path] = File.join(@options[:bundle_file_path].split("/")[0..-2])
+  end
+
+  # So that the bundler can be configured and run in two different places ... like Sass
+  module_function :configure
+
+  class Basic
+
+    def initialize
+      @bundle_options = Fusion.instance_variable_get('@options') 
+      @bundle_configs = YAML::load(File.open(@bundle_options[:bundle_file_path]))
+    end
+
+    def run
+      start = Time.now
+
+      @bundle_configs.each do |config|
+        bundle(config)
+      end
+
+      puts "Javascript Reloaded #{@bundle_configs.size} bundle(s) (#{Time.now - start}s)"
+    end
+
+    def gather_files(config)
+      input_files = []
+
+      if(config[:input_files])
+        config[:input_files].each do |input_file|
+          input_files << File.join(@bundle_options[:project_path], input_file)
+        end
+      end
+
+      if (config[:input_directory])
+        directory = File.join(@bundle_options[:project_path],config[:input_directory])
+
+        file_names = Dir.open(directory).entries.sort.find_all {|filename| filename.end_with?(".js") }
+
+        input_files += file_names.collect do |file_name|
+          File.join(directory, file_name)
+        end
+      end
+
+      input_files.uniq
+    end
+
+    def get_output_file(config)
+      raise Exception.new("Undefined js bundler output file") if config[:output_file].nil?
+      File.join(@bundle_options[:project_path], config[:output_file])
+    end
+
+  end
+
+  class Quick < Basic
+
+    def bundle(config)
+      js = []
+
+      input_files = gather_files(config)      
+
+      input_files.each do |input_file|
+        js << File.open(input_file, "r").read
+      end
+      
+      js = js.join("\n")
+      
+      File.open(get_output_file(config), "w") { |f| f << js }
+    end
+
+  end
+
+  class Optimized < Basic
+    
+    def bundle(config)
+      options = []
+
+      options << ["js_output_file", get_output_file(config)]
+      options << ["compilation_level", "SIMPLE_OPTIMIZATIONS"]
+
+      gather_files(config).each do |input_file|
+        options << ["js", input_file]
+      end
+
+      options.collect! do |option|
+        "--#{option[0]} #{option[1]}"
+      end
+
+      jar_file = File.join(__FILE__.split("/")[0..-3].join("/"), "/compiler/compiler.jar")
+      cmd = "java -jar #{jar_file} #{options.join(" ")}"
+      io = IO.popen(cmd, "w")
+      io.close
+
+      raise Exception.new("Error creating bundle: #{get_output_file(config)}") unless $?.exitstatus == 0
+    end
+    
+  end
+
+end
