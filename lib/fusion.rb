088fb6f59ca0b1a44cdfc5724a07c91f7786207e
Sean Jezewski <sean@moovweb.com>
2011-07-26 20:33:04 -0700

    Added the 'Pretty' compilation mode. This is when all I want is the output to be readable. Its not as fast as the quick mode, but I'll use it for the human readable bundles that uranium creates

--- lib/fusion.rb
+++ lib/fusion.rb
@@ -106,6 +106,31 @@ module Fusion
 
   end
 
+  class Pretty < Basic
+    def bundle(config)
+      options = []
+
+      options << ["js_output_file", get_output_file(config)]
+      options << ["compilation_level", "WHITESPACE_ONLY"]
+      options << ["formatting", "PRETTY_PRINT"]
+
+      gather_files(config).each do |input_file|
+        options << ["js", input_file]
+      end
+
+      options.collect! do |option|
+        "--#{option[0]} #{option[1]}"
+      end
+
+      jar_file = File.join(__FILE__.split("/")[0..-3].join("/"), "/compiler/compiler.jar")
+      cmd = "java -jar #{jar_file} #{options.join(" ")}"
+      io = IO.popen(cmd, "w")
+      io.close
+
+      raise Exception.new("Error creating bundle: #{get_output_file(config)}") unless $?.exitstatus == 0
+    end
+  end
+
   class Quick < Basic
 
     def bundle(config)
