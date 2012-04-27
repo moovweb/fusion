e1d5d6a9b0e93ab9b6a2b7d021e6de12e7d31d46
Zhigang Chen <zhigang.chen@moovweb.com>
2012-04-27 13:32:18 -0700

    stop the test when fails

--- src/fusion/sanity_test.go
+++ src/fusion/sanity_test.go
@@ -8,16 +8,16 @@ import(
 func TestYAMLLoading(t *testing.T) {
 	bundles, _, _ := getBundles("test/example-bundle.yml")
 	if len(bundles) != 1 {
-		t.Errorf("there should be one bundle entry\n")
+		t.Fatalf("there should be one bundle entry\n")
 	}
 	b := bundles[0].(map[interface{}]interface{})
 	input_files := b[":input_files"].([]interface{})
 	if len(input_files) != 1 {
-		t.Errorf("there should be 1 input file\n")
+		t.Fatalf("there should be 1 input file: %d\n", len(input_files))
 	}
 	name := input_files[0].(string)
 	if name != "http://d1topzp4nao5hp.cloudfront.net/uranium-upload/0.1.23/uranium.js" {
-		t.Errorf("the input file name does not match\n")
+		t.Fatalf("the input file name does not match\n")
 	}
 }
 
