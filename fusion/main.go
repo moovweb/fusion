f5ce642a39c0e8de0c984e5139506db553108fd5
Jose Espinosa <jose.espinosa@moovweb.com>
2012-12-05 13:10:42 -0800

    Add mesage about println use

--- fusion/main.go
+++ fusion/main.go
@@ -13,6 +13,7 @@ func main() {
 	bundler, err := fusion.NewQuickBundler(os.Args[1], logger)
 
 	if err != nil {
+		// We call println since fmt.Println might not be printed out.
 		println("Error:", err.Error())
 	} else {
 		bundler.Run()
